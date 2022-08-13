f00fdae2b9e295a8b9ff03688c615cf6a41a8841
evanjpowell <91023012+evanjpowell@users.noreply.github.com>
2022-08-13 10:49:27 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+class metro:
+    def __init__(self, size, num_stops):
+        self.size = size
+        self.num_stops = num_stops
+
+    def make_stops(self):
+        pass
+
+class line:
+    def __init__(self, crosstown):
+        self.crosstown = crosstown
